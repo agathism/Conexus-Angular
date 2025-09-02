c58a8e2539e98a4f43ff352d5839de2b43fa9d7e
agathism <soumare.aicha13@gmail.com>
2025-09-02 17:47:23 +0200

    deleting in login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,5 +9,4 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
-}
+  }
